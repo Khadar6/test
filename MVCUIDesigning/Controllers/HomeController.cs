352c4050aa45dda2a1c903c3b97fa404346a0116
DESKTOP-JMMNLOE\DesIDEA <jagadeesh.nayakoti@brother.com>
2021-07-26 17:51:07 +0530

    Initial Checking

---
+++ MVCUIDesigning/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCUIDesigning.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        public ViewResult Validate(FormCollection fc)
+        {
+            if (fc["txtName"] == "Raju" && fc["txtPwd"] == "admin#123")
+            {
+                Session["User"] = fc["txtName"];//All Pages
+                return View("Success");
+            }
+            else
+            {
+                ViewBag.User = fc["txtName"];//Only one page
+                return View("Failure");
+            }
+
+        }
+    }
+}
